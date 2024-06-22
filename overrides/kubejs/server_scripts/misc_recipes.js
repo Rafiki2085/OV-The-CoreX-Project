c7484673999e08365df2367929328faaf371d9f1
Dalton <datadalton.business@gmail.com>
2024-06-22 14:08:08 -0400

    0.0.5
    Squ

---
+++ overrides/kubejs/server_scripts/misc_recipes.js
@@ -0,0 +1,19 @@
+// priority: 0
+
+// Visit the wiki for more info - https://kubejs.com/
+
+ServerEvents.recipes(event => {
+
+  event.shapeless(Item.of('javd:portal_block'), [ // arg 1: output
+    'minecraft:dirt', //arg 2: the array of inputs
+    
+  ])
+
+    //Bamboo blocks
+    //transmute(['quark:bamboo_block', 'thermal:bamboo_block'])
+    //transmute(['thermal:bamboo_block', 'quark:bamboo_block'])
+
+
+
+})
+
